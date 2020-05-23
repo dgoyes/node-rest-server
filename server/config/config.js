6b530ef8e48d3440225f713009a2761eed42658c
dgoyes <dgb100@gmail.com>
2020-05-22 20:07:53 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://dgoyes:vArsQZngoDGUX0QP@cluster0-j5de0.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
