7628a324127876f86bc17ff397ff485bea683668
dgoyes <dgb100@gmail.com>
2020-05-28 20:26:41 -0500

    Login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express();
 const Usuario = require('../models/usuario');
 
+const { verificaToken, verificaAdminRole } = require('../middlewares/autenticacion');
+
+
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, function(req, res) {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -38,7 +41,7 @@ app.get('/usuario', function(req, res) {
         })
 })
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdminRole], function(req, res) {
 
     let body = req.body;
 
@@ -67,7 +70,7 @@ app.post('/usuario', function(req, res) {
 
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdminRole], function(req, res) {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
@@ -116,7 +119,7 @@ app.delete('/usuario/:id', function(req, res) {
 */
 
 // ACTUALIZA EL ESTADO DEL REGISTRO DE LA BASE DE DATOS
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdminRole], function(req, res) {
     let id = req.params.id;
     let cambiaEstado = {
         estado: false
